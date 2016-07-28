48b491116dfe937522321d7ecd4976c299368c10
patyogesh <patyogesh@gmail.com>
2016-07-27 20:27:50 -0700

    Reading secret keys from local filesystem now.

--- samples/GatorStream/streamCore.py
+++ samples/GatorStream/streamCore.py
@@ -2,10 +2,21 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 
-ckey = '<comsumer_key>'
-csecret = '<consumer_secret>'
-atoken = '<authorization_token>'
-asecret = '<authorization_secret>'
+secret_keys = []
+# secret_keys[0] : ckey = '<comsumer_key>'
+# secret_keys[1] : csecret = '<consumer_secret>'
+# secret_keys[2] : atoken = '<authorization_token>'
+# secret_keys[3] : asecret = '<authorization_secret>'
+
+def keys_io():
+    key_file = open('/home/yogesh/.twit_keys', 'r+')
+    for key in range(1,5):
+        secret_keys.append(key_file.readline().split(" = ")[1])
+
+    for i in secret_keys:
+        print i
+
+keys_io()
 
 class Listener(StreamListener):
 
@@ -18,8 +29,8 @@ class Listener(StreamListener):
 
 
 
-auth = OAuthHandler(ckey, csecret)
-auth.set_access_token(atoken, asecret)
+auth = OAuthHandler(secret_keys[0], secret_keys[1])
+auth.set_access_token(secret_keys[2], secret_keys[3])
 
 twitterstream = Stream(auth, Listener())
 twitterstream.filter(track=['Yogesh'])
