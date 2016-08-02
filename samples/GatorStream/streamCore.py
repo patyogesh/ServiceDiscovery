448a3a59262820ba79b5d46983dfe81b1b86e1bf
patyogesh <patyogesh@gmail.com>
2016-08-01 22:48:58 -0700

    Added Kafka Producer & Consumer

--- samples/GatorStream/streamCore.py
+++ samples/GatorStream/streamCore.py
@@ -1,6 +1,9 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+from kafka import KafkaProducer
+from kafka import KafkaConsumer
+import threading
 import json
 
 '''
@@ -24,24 +27,43 @@ def keys_io():
         secret_keys.append(key_file.readline().split("=")[1].strip())
 
 
-keys_io()
-
-
 class Listener(StreamListener):
-
+    prod = KafkaProducer(bootstrap_servers='localhost:9092')
     def on_data(self, raw_data):
         #print raw_data
         tweet_text = json.loads(raw_data)
         json.dumps(tweet_text, sort_keys=True, indent=4)
         print "Tweet: ", tweet_text['text']
+        self.prod.send('topic1', b'msg1')
         return True
 
     def on_error(self, status_code):
         print status_code
 
 
-auth = OAuthHandler(secret_keys[0], secret_keys[1])
-auth.set_access_token(secret_keys[2], secret_keys[3])
+def consumer():
+    print 'starting consumer'
+    cons = KafkaConsumer(bootstrap_servers='localhost:9092',
+                             auto_offset_reset='earliest')
 
-twitterstream = Stream(auth, Listener())
-twitterstream.filter(track=['Olympics'])
+    cons.subscribe(['topic1'])
+
+    for msg in cons:
+        print msg
+
+
+def main():
+    keys_io()
+    auth = OAuthHandler(secret_keys[0], secret_keys[1])
+    auth.set_access_token(secret_keys[2], secret_keys[3])
+
+    twitterstream = Stream(auth, Listener())
+    twitterstream.filter(track=['DonaldTrump'])
+
+    t = threading.Thread(target=consumer)
+    t.daemon = True
+    t.start()
+
+
+if __name__ == "__main__":
+    main()
