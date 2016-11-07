106f47e6774a00d2379b6ad56881b1102c54ed8f
bhavneshgugnani <bhavnesh.gugnani@gmail.com>
2016-11-07 07:26:28 -0800

    Correcting how the subsequent producer/broker instances are spawned and service is scaled by making first command as daemon. This will make sure second command of scale is executed. Earlier, first command was not daemon causing secind command never to be executed.

--- launch-compose-on-swarm.sh
+++ launch-compose-on-swarm.sh
@@ -85,7 +85,7 @@ startapp() {
   # Point docker env to swarm master node
   eval "$(docker-machine env --swarm $master)"
   # Start application
-  docker-compose -f docker-pull.yml up
+  docker-compose -f docker-pull.yml up -d && docker-compose -f docker-pull.yml scale producer=2 broker=2
   # Point docker back to local env
   eval "$(docker-machine env -u)"
 }
