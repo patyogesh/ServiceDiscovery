24c09c25fd6a6ce89a35dad771ac820ea0eb760f
bhavneshgugnani <bhavnesh.gugnani@gmail.com>
2016-11-19 19:17:13 -0800

    Fixing mongo DB for storage in cluster.

--- samples/mongo/mongoclient.py
+++ samples/mongo/mongoclient.py
@@ -2,14 +2,13 @@
 from pymongo import MongoClient
 import datetime
 
-client = MongoClient()
 client = MongoClient('localhost', 27017)
 mydb = client['test_database_1'] # get database
 my_collection = mydb['test-database'] # get collection
 
 myrecord = {"author": "Duke", "title" : "PyMongo 101", "tags" : ["MongoDB", "PyMongo", "Tutorial"], "date" : datetime.datetime.utcnow() }
 
-#record_id = my_collection.insert(myrecord)
+record_id = my_collection.insert(myrecord)
 print mydb.collection_names()
 print my_collection.find_one()
 
